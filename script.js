b008177718f879401a3a13a1e0ac6c99dc1c8c83
anil200721 <4u2banildav@gmail.com>
2025-01-30 19:49:01 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function moveRandomE1(elm) {
     elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
 }
 
-const moveRandom = document.querySelector("#moveRandom");
+const moveRandom = document.querySelector("#move-random");
 
 moveRandom.addEventListener("mouseenter", function(e) {
     moveRandomE1(e.target);
